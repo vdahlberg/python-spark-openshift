76472be420d7ac4fc137606b390e072df4fc5e69
Victor Dahlberg <victor.dahlberg@capgemini.com>
2018-11-07 16:44:34 +0100

    cleanup

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os
 from os import environ
 from pyspark.sql import SparkSession
 #import mysql.connector
-#from azure.common.credentials import ServicePrincipalCredentials
-#from azure.datalake.store import core, lib, multithread
 
 #db = mysql.connector.connect(host=environ.get("MYSQL_HOST"), user=environ.get("MYSQL_USER"), passwd=environ.get("MYSQL_PWD"))
 
@@ -20,21 +18,9 @@ storage_account_access_key = environ.get("STORAGE_KEY")
 spark = SparkSession.builder.appName('wrangler').config("spark.hadoop.fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem").config("fs.wasbs.impl", "org.apache.hadoop.fs.azure.NativeAzureFileSystem").config("fs.azure.account.key."+storage_account_name+".blob.core.windows.net", storage_account_access_key).getOrCreate()
 			
 
-
 file_location = "abfss://testshare/"
-print(storage_account_access_key)
-
 
 df = spark.read.format("csv").options(header='true',inferschema='true',sep=";").load("wasbs://testshare@svvpocdlgen2.blob.core.windows.net/1900116_20180306000000-20180331235900.csv")
 print(df)
 spark.stop()
 
-#r = r.map(lambda x: x * 2)
-#r.collect()
-
-#auth
-#token = lib.auth(tenant_id = environ.get("AZ_TENANT_ID"), client_secret = environ.get("AZ_CLIENT_SECRET"), client_id = environ.get("AZ_CLIENT_ID"))
-
-# create ADLS file system client
-
-#adls = core.AzureDLFileSystem(token, store_name = environ.get("AZ_STORE_NAME"))
