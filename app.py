c222e8fa414c9ac2f9e41aea9ff65a15540128da
Victor Dahlberg <victor.dahlberg@capgemini.com>
2018-11-07 15:44:30 +0100

    config

--- app.py
+++ app.py
@@ -13,18 +13,26 @@ from pyspark.sql import SparkSession
 #for x in cursor:
 #    print(x)
 
-spark = SparkSession.builder.appName('wrangler').getOrCreate()
-#r = spark.sparkContext.parallelize(range(10))
 storage_account_name = "svvpocdlgen2"
 storage_account_access_key = environ.get("STORAGE_KEY")
 
+
+spark = SparkSession.builder.appName('wrangler')
+			.config("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
+			.config("spark.hadoop.fs.wasb.imlp", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
+			.config("fs.azure.account.key."+storage_account_name+".blob.core.windows.net", storage_account_access_key).
+			getOrCreate()
+			
+
+
 file_location = "abfss://testshare/"
-spark.conf.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
 print(storage_account_access_key)
-spark.conf.set("fs.azure.account.key."+storage_account_name+".blob.core.windows.net", storage_account_access_key)
+
+
 df = spark.read.format("csv").options(header='true',inferschema='true',sep=";").load("wasbs://testshare@svvpocdlgen2.blob.core.windows.net/1900116_20180306000000-20180331235900.csv")
 print(df)
 spark.stop()
+
 #r = r.map(lambda x: x * 2)
 #r.collect()
 
